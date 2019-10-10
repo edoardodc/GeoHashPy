b93448b514a4734937f5db4ce766fc5d6e67134a
Edoardo de Cal <edoardo.decal@gmail.com>
2019-10-10 08:55:09 +0200

    added files

---
+++ FountainsDataPythonProgram.py
@@ -0,0 +1,19 @@
+import json
+
+a = 0
+
+with open('FountainData.json', 'r') as data_file:
+    data = json.load(data_file)
+
+for element in data:
+    if 'id' in element:
+         a = a + 1
+         print(element['id'])
+         print(a)
+         del element['id']
+
+
+with open('FountainData.json', 'w') as data_file:
+    myData = json.dump(data, data_file, indent=4)
+
+print('Finished!')
