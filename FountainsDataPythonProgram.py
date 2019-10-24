91bc79d2fd308f6b5ae6850eaf19fb00e5d94054
Edoardo de Cal <edoardo.decal@gmail.com>
2019-10-24 19:40:52 +0200

    added some really delicious code

--- FountainsDataPythonProgram.py
+++ FountainsDataPythonProgram.py
@@ -1,4 +1,21 @@
 import json
 import pygeohash as pgh
 
+
 print(pgh.encode(19.4354778, -99.1364789, precision = 10))
+
+with open('SmallData.json', 'r') as outfile:
+    data = json.load(outfile)
+    for element in data:
+        latitude = element["l"][0]
+        longitude = element["l"][1]
+        geoHash = pgh.encode(latitude, longitude, precision = 10)
+        print(geoHash)
+        element["g"] = geoHash
+        
+with open('SmallData.json', 'w') as outfile:
+    json.dump(data, outfile, indent=4)
+
+
+
+print(data)
