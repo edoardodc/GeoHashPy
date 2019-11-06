f8543c667a7057c23a891935b8ac25d569abb7a2
Edoardo de Cal <edoardo.decal@gmail.com>
2019-11-06 22:33:50 +0100

    fixed some problems

--- FountainsDataPythonProgram.py
+++ FountainsDataPythonProgram.py
@@ -3,6 +3,7 @@ import pygeohash as pgh
 
 
 print(pgh.encode(19.4354778, -99.1364789, precision = 10))
+total = 41906
 
 with open('SmallData.json', 'r') as outfile:
     data = json.load(outfile)
@@ -10,8 +11,10 @@ with open('SmallData.json', 'r') as outfile:
         latitude = element["l"][0]
         longitude = element["l"][1]
         geoHash = pgh.encode(latitude, longitude, precision = 10)
-        print(geoHash)
         element["g"] = geoHash
+        total = total - 1
+        print(total)
+        print(geoHash)
         
 with open('SmallData.json', 'w') as outfile:
     json.dump(data, outfile, indent=4)
@@ -19,3 +22,4 @@ with open('SmallData.json', 'w') as outfile:
 
 
 print(data)
+
