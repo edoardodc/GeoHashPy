a2b6183359526cc0be646d6cf90f6316235d56a5
Edoardo de Cal <edoardo.decal@gmail.com>
2019-12-16 00:32:24 +0100

    completed

--- FountainsDataPythonProgram.py
+++ FountainsDataPythonProgram.py
@@ -1,21 +1,47 @@
 import json
+import pygeohash as pgh
 
 count = 0
+latitude = 0.00
+longitude = 0.00
+geoHash = ''
 
 with open('data.json', 'r') as outfile:
     data = json.load(outfile)
-    myData = data["Fountains"]
-    for element in myData:
+    for element in data:
         count += 1
         print(count)
         element.pop('properties', None)
         element.pop('type', None)
         element.pop('id', None)
+        
         geometry = element['geometry']
         for el in list(geometry):
-            if 'type' == el:
-                del geometry['type']
+            
+            coordinate = geometry['coordinates']
+            latitude = coordinate[0]
+            longitude = coordinate[1]
 
+        print(latitude)
+        print(longitude)
+        element.pop('geometry', None)
+
+        l =  {'l': [latitude, longitude]}
+        print(l)
+        element.update(l)
+
+        geoHash = pgh.encode(latitude, longitude, precision = 10)
+
+        geoHash = {'g': geoHash}
+        element.update(geoHash)
+
+            #if el == 'type':
+                #del geometry['type']
+           
+            
+
+
+        
         
 
         
