856c03538bdfa2c3b270d6fc1775a6219df952e9
Edoardo de Cal <edoardo.decal@gmail.com>
2019-12-16 23:30:13 +0100

    fixed some bugs

--- FountainsDataPythonProgram.py
+++ FountainsDataPythonProgram.py
@@ -6,11 +6,13 @@ latitude = 0.00
 longitude = 0.00
 geoHash = ''
 
+perc = 0
+totElements = 14475
+
 with open('data.json', 'r') as outfile:
     data = json.load(outfile)
     for element in data:
         count += 1
-        print(count)
         element.pop('properties', None)
         element.pop('type', None)
         element.pop('id', None)
@@ -21,34 +23,23 @@ with open('data.json', 'r') as outfile:
             coordinate = geometry['coordinates']
             latitude = coordinate[0]
             longitude = coordinate[1]
-
-        print(latitude)
-        print(longitude)
+            
         element.pop('geometry', None)
 
         l =  {'l': [latitude, longitude]}
-        print(l)
         element.update(l)
 
         geoHash = pgh.encode(latitude, longitude, precision = 10)
 
         geoHash = {'g': geoHash}
         element.update(geoHash)
-
-            #if el == 'type':
-                #del geometry['type']
-           
-            
+        perc = (100*count)/totElements
+        print("{:.2f} %".format(perc))
 
 
-        
-        
 
-        
+                       
 with open('new.json', 'w') as outfile:
     json.dump(data, outfile, indent=4)
-
-
-
 
 
